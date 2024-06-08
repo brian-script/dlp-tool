8a8abd5b61b1cc88f99f1ca8f4ac61bf016b6004
brian <brianarguello358@gmail.com>
2024-06-07 21:51:24 -0500

    agregando opciones

--- audio_dl.py
+++ audio_dl.py
@@ -1,12 +1,21 @@
 import yt_dlp
 
+link = input("Ingresa el link del video: ")
 
-link = "https://youtu.be/ehfLnF5p8i0?si=cPdu8f_DbWw9mNRu"
+print("quieres descargar el video o solo el audio?: \n1- video(mp4)\n2- audio(mp3)")
+opcion = int(input("elige una opcion: "))
 
-formato_salida = "mp3"
-calidad = "192"
+if opcion == 1:
+    ydl_opts = {
+            "format": "best",}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
 
-ydl_opts = {
+elif opcion == 2:
+    formato_salida = "mp3"
+    calidad = "192"
+
+    ydl_opts = {
         "format": "bestaudio/best",
         "postprocessors": [{
             "key": "FFmpegExtractAudio",
@@ -16,5 +25,5 @@ ydl_opts = {
             "outtmpl": "%(title)s.%(ext)s",
         }
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([link])
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
