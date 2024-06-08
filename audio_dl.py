faf5102db971078a4b7e380b032d398f82154264
brian <brianarguello.fix@gmail.com>
2024-06-08 16:22:23 -0400

    agregando opciones

--- audio_dl.py
+++ audio_dl.py
@@ -1,20 +1,29 @@
 import yt_dlp
 
 
-link = "https://youtu.be/ehfLnF5p8i0?si=cPdu8f_DbWw9mNRu"
+link = input("Ingresa el link dek video: ")
+print("deseas descargar el video o solo el audio?: \n1- video(webm)\n2- audio(mp3)")
+opcion = input("elige una opcion: ")
 
-formato_salida = "mp3"
-calidad = "192"
+if opcion == "1":
+    ydl_opts = {
+            "format": "bestvideo/bestaudio",}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
 
-ydl_opts = {
-        "format": "bestaudio/best",
-        "postprocessors": [{
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": formato_salida,
-            "preferredquality": calidad, 
-            }],
-            "outtmpl": "%(title)s.%(ext)s",
-        }
+elif opcion == "2":
+    formato_salida = "mp3"
+    calidad = "192"
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([link])
+    ydl_opts = {
+            "format": "bestaudio/best",
+            "postprocessors": [{
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": formato_salida,
+                "preferredquality": calidad, 
+                }],
+                "outtmpl": "%(title)s.%(ext)s",
+            }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
